8c8503b20ac80fede1587f691fa585cd90bbdd10
robertfkillion@gmail.com <robertfkillion@gmail.com>
2021-11-22 10:34:53 -0500

    fix basic components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import AuthenticatedApp from "./components/AuthenticatedApp";
+import UnauthenticatedApp from "./components/unauthenticated/UnauthenticatedApp";
 
 function App() {
+  const [user,setUser] = useState({})
+  //fetch user data, set or redirect to unauthenticated
+  useEffect(()=>{
+    setUser({id: 1,username: "CurrentUser"}) //placeholder
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {user.id ? <AuthenticatedApp user={user}/> : <UnauthenticatedApp />}
     </div>
   );
 }
