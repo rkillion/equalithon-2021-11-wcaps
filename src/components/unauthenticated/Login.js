3d412b4abdf9282fb4372c164609c71e63c74966
robertfkillion@gmail.com <robertfkillion@gmail.com>
2021-11-22 17:13:16 -0500

    added login page

--- src/components/unauthenticated/Login.js
+++ src/components/unauthenticated/Login.js
@@ -1,10 +1,8 @@
-import Login from "./Login";
-
-export default function UnauthenticatedApp() {
+export default function Login() {
     return (
-        <div>
+        <div className="login-page">
             {/* to preview the unauthenticated app and the login page, change the id from 1 to null in the setUser in useEffect on the App.js file. This will simulate not having a logged in user. */}
-            <Login />
+            Login page code here
         </div>
     )
 }
