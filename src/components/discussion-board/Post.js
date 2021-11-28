de95274582e3ae552db1abbfd29d14ca56c4ead9
puja.agarwal2006@gmail.com <puja.agarwal2006@gmail.com>
2021-11-28 13:43:29 -0500

    stylized the discussion-board

--- src/components/discussion-board/Post.js
+++ src/components/discussion-board/Post.js
@@ -6,11 +6,13 @@ export default function Post({ post }) {
         <div className="post-and-comments">
             <div className="post">
                 <div className="post-header">
+                  <div className="photo-and-title" >
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
                         <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
                         <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
                     </svg>
                     <h3 className="post-title">{post.title}</h3>
+                    </div>
                     <span className="post-date">{displayDate(post.created_at)}</span>
                 </div>
                 <div className="post-content">
